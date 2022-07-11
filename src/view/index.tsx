6daf7795dc106e6959537a9f778c803a378a45e1
tuphan-dn <tuphan@descartes.network>
2022-07-11 19:02:06 +0700

    first commit

--- src/view/index.tsx
+++ src/view/index.tsx
@@ -1,19 +1,75 @@
-import EmbededView from '@sentre/embeded-view'
+import { useState, useEffect } from 'react'
+import { Gateway } from '@sentre/connector'
 
-import configs from 'configs'
-
-const {
-  manifest: { appId },
-} = configs
+import { Button, Card, Col, Input, Row } from 'antd'
 
 const View = () => {
+  const [tmpSrc, setTmpSrc] = useState('')
+  const [tmpAppId, setTmpAppId] = useState('')
+  const [src, setSrc] = useState('https://sentre.io/')
+  const [appId, setAppId] = useState('my-app-id')
+
+  useEffect(() => {
+    const gateway = new Gateway(window.sentre.wallet)
+    return gateway.terminate
+  }, [])
+
   return (
-    <EmbededView
-      appId={appId}
-      src={'https://sentre.io/'}
-      title="Please replace the src and title to your original DApp."
-      wallet={window.sentre.wallet}
-    />
+    <Row gutter={[12, 12]}>
+      <Col span={12}>
+        <Input
+          value={tmpAppId}
+          onChange={(e) => setTmpAppId(e.target.value || '')}
+          placeholder="my-app-id"
+          suffix={
+            <Button
+              type="text"
+              size="small"
+              onClick={() => setAppId(tmpAppId)}
+              disabled={!tmpAppId || tmpAppId === appId}
+              style={{ marginRight: -7 }}
+            >
+              Save
+            </Button>
+          }
+        />
+      </Col>
+      <Col span={12}>
+        <Input
+          value={tmpSrc}
+          onChange={(e) => setTmpSrc(e.target.value || '')}
+          placeholder="http://localhost:3000"
+          suffix={
+            <Button
+              type="text"
+              size="small"
+              onClick={() => setSrc(tmpSrc)}
+              disabled={tmpSrc === src}
+              style={{ marginRight: -7 }}
+            >
+              Save
+            </Button>
+          }
+        />
+      </Col>
+      <Col span={24}>
+        <Card bodyStyle={{ padding: 16, height: 'calc(100vh - 144px)' }}>
+          <iframe
+            id={appId + '-iframe'}
+            src={src}
+            title="Senhub Connector Tester."
+            style={{
+              height: '100%',
+              width: '100%',
+              border: 'none',
+              borderRadius: 8,
+            }}
+            loading="lazy"
+            allowFullScreen
+          />
+        </Card>
+      </Col>
+    </Row>
   )
 }
 
